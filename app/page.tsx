92c0abb05a39086d55503e50d9e0c2fad8c5222b
thiagonmiziara <thiagonmiziara@gmail.com>
2025-02-12 17:11:41 -0300

    feat: add component header

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
+import Header from "./components/landing-page/header";
 import Hero from "./components/landing-page/hero";
 
 export default function Home() {
   return (
     <div className='max-w-7xl mx-auto'>
+      <Header />
       <Hero />
-      {/* <Header />
+      {/* 
       <VideoExplanation />
       <Pricing />
       <FAQ /> */}
